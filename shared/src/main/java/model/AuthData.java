880bd8b55d4670e9e33ad17c876821621bab318f
KCcooldude11 <kaseylightheart@yahoo.com>
2024-02-25 13:53:26 -0700

    model files

---
+++ shared/src/main/java/model/AuthData.java
@@ -0,0 +1,14 @@
+package model;
+
+public class AuthData {
+    private String authToken;
+    private String username;
+
+    public AuthData(String authToken, String username) {
+        this.authToken = authToken;
+        this.username = username;
+    }
+
+    // Getters and Setters
+    // Implement getters and setters here
+}
